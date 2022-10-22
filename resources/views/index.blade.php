5aa981c6ac67f32313584c4b4987e746b91baead
Shofi Setiawan <hey.shofi@gmail.com>
2022-10-22 21:33:21 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -51,7 +51,7 @@
                     <div class="col-md-6">
                         <div class="card" style="width: 110px; border: 0;">
                             <a href="">
-                                <img src="{{ asset('assets/img/kategori/Stetoskop.png') }}" href=""
+                                <img src="{{ asset('assets/img/shop/alatdokter.png') }}" href=""
                                     style="width: 100px; height: 100px;">
                             </a>
                             <p>
@@ -62,7 +62,7 @@
                     <div class="col-md-6">
                         <div class="card" style="width: 110px; border: 0;">
                             <a href="">
-                                <img src="{{ asset('assets/img/kategori/bantu-dengar.png') }}"
+                                <img src="{{ asset('assets/img/shop/alatbantudengar.png') }}"
                                     style="width: 70px; height: 70px; margin-top: 10px; margin-left: 15px;">
                             </a>
                             <p>
@@ -73,7 +73,7 @@
                     <div class="col-md-6">
                         <div class="card" style="width: 110px; border: 0;">
                             <a href="">
-                                <img src="{{ asset('assets/img/kategori/bantu-jalan.jpg') }}"
+                                <img src="{{ asset('assets/img/shop/alatbantujalan.png') }}"
                                     style="width: 100px; height: 80px;">
                             </a>
                             <p>
@@ -84,7 +84,7 @@
                     <div class="col-md-6">
                         <div class="card" style="width: 110px; border: 0;">
                             <a href="">
-                                <img src="{{ asset('assets/img/kategori/rapid.jpg') }}"
+                                <img src="{{ asset('assets/img/shop/rapidtest.png') }}"
                                     style="width: 100px; height: 100px;">
                             </a>
                             <p>
@@ -95,7 +95,7 @@
                     <div class="col-md-6">
                         <div class="card" style="width: 110px; border: 0;">
                             <a href="">
-                                <img src="{{ asset('assets/img/kategori/p3k.png') }}" style="width: 100px; height: 100px;">
+                                <img src="{{ asset('assets/img/shop/p3k.png') }}" style="width: 100px; height: 100px;">
                             </a>
                             <p>
                                 <center>Alat P3K</center>
@@ -105,8 +105,7 @@
                     <div class="col-md-6">
                         <div class="card" style="width: 110px; border: 0;">
                             <a href="">
-                                <img src="{{ asset('assets/img/kategori/model.png') }}"
-                                    style="width: 100px; height: 100px;">
+                                <img src="{{ asset('assets/img/shop/model.png') }}" style="width: 100px; height: 100px;">
                             </a>
                             <p>
                                 <center>Model Kerangka Manusia</center>
@@ -116,7 +115,7 @@
                     <div class="col-md-6">
                         <div class="card" style="width: 110px; border: 0;">
                             <a href="">
-                                <img src="{{ asset('assets/img/kategori/kursiroda.jpeg') }}"
+                                <img src="{{ asset('assets/img/shop/kursiroda.png') }}"
                                     style="width: 100px; height: 100px;">
                             </a>
                             <p>
@@ -127,7 +126,7 @@
                     <div class="col-md-6">
                         <div class="card" style="width: 110px; border: 0;">
                             <a href="">
-                                <img src="{{ asset('assets/img/kategori/termometer.png') }}"
+                                <img src="{{ asset('assets/img/shop/termometer.png') }}"
                                     style="width: 100px; height: 100px;">
                             </a>
                             <p>
@@ -157,7 +156,8 @@
                         <div class="single-latest-news">
                             <a href="#">
                                 <div>
-                                <img class="latest-news-bg img-fluid" style="" src="{{ Storage::url('public/artikel/') . $artikel->foto }}" alt="">
+                                    <img class="latest-news-bg img-fluid" style=""
+                                        src="{{ Storage::url('public/artikel/') . $artikel->foto }}" alt="">
                                 </div>
                             </a>
                             <div class="news-text-box">
@@ -168,20 +168,19 @@
                                 <p class="excerpt">
                                     {!! $artikel->deskripsi !!}
                                 </p>
-                                <a type="button" class="latest-brand1" data-toggle="modal"
-                                    data-target=".bd-example-modal-lg-{{ $key }}">
+                                <a type="button" class="latest-brand1" data-toggle="modal" data-target="#exampleModal-{{ $artikel->id }}">
                                     Read More...
                                 </a>
                             </div>
                         </div>
                     </div>
 
-                    <div class="modal fade bd-example-modal-lg-{{ $key }}" tabindex="-1" role="dialog"
-                        aria-labelledby="myLargeModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-lg">
+                    <div class="modal fade" id="exampleModal-{{ $artikel->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
+                        aria-hidden="true">
+                        <div class="modal-dialog modal-lg modal-dialog-scrollable">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLongTitle">{{ $artikel->judul }}</h5>
+                                    <h5 class="modal-title" id="exampleModalLabel">{{ $artikel->judul }}</h5>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
@@ -189,7 +188,9 @@
                                 <div class="modal-body text-center">
                                     <img src="{{ Storage::url('public/artikel/') . $artikel->foto }}" alt="">
                                     <br><br>
-                                    {!! $artikel->deskripsi !!}
+                                    <div>
+                                        {!! $artikel->deskripsi !!}
+                                    </div>
                                 </div>
                             </div>
                         </div>
